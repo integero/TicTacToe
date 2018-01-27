38e8bd5f3d9e0eee0ed46345159cca1d5d921e23
Valeryi Tselishchev <var.tsel@gmail.com>
2018-01-28 00:45:31 +0300

    Good SmField

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        SmallField sf = new SmallField();
-        sf.getSmallField()[0][0].setPlus();
-        sf.getSmallField()[1][1].setPlus();
-        sf.getSmallField()[2][2].setPlus();
+        SmField sf = new SmField();
+        sf.setXij(0, 0);
+        System.out.println(sf.getSmallState());
+        sf.setXij(1, 1);
+        System.out.println(sf.getSmallState());
+        sf.setXij(2, 2);
+        System.out.println(sf.getSmallState());
+        sf.setXij(0, 2);
+        System.out.println(sf.getSmallState());
+        sf.setOij(1, 2);
+        System.out.println(sf.getSmallState());
+        sf.setOij(1, 2);
         for (int i = 0; i < sf.getSequence().length; i++) {
             System.out.print(sf.getSequence()[i]+"  ");
         }
--- src/main/java/Sta.java
+++ src/main/java/Sta.java
@@ -1,4 +1,10 @@
 public class Sta {
-    private int[][][] cellSeq;
+    public final static int[][][] cellSeq = new int[][][]{
+            {{1, 4, 7},     {1, 6},         {0, 1, 5}},
+            {{2, 7},        {0, 2, 4, 6},   {2, 5}},
+            {{0, 3, 7},     {3, 6},         {3, 4, 5}}};
 
+    int[] getSeq(int i, int j) {
+        return cellSeq[i][j];
+    }
 }
