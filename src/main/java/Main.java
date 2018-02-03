e05fc103aeba2abdedc13130e02d9ce5e7a88a63
Valeryi Tselishchev <var.tsel@gmail.com>
2018-02-03 20:23:42 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,29 @@
+package main.java;
+
+import static javafx.application.Application.launch;
+
 public class Main {
     public static void main(String[] args) {
+        launch(Game.class);
         SmField sf = new SmField();
         sf.setXij(0, 0);
-        System.out.println(sf.getSmallState());
-        sf.setXij(1, 1);
-        System.out.println(sf.getSmallState());
-        sf.setXij(2, 2);
-        System.out.println(sf.getSmallState());
+        System.out.println(sf.getStateSm());
+        sf.setXij(0, 1);
+        System.out.println(sf.getStateSm());
         sf.setXij(0, 2);
-        System.out.println(sf.getSmallState());
+        System.out.println(sf.getStateSm());
+        sf.setOij(1, 0);
+        System.out.println(sf.getStateSm());
+        sf.setOij(1, 1);
+        System.out.println(sf.getStateSm());
         sf.setOij(1, 2);
-        System.out.println(sf.getSmallState());
-        sf.setOij(1, 2);
+        System.out.println(sf.getStateSm());
         for (int i = 0; i < sf.getSequence().length; i++) {
             System.out.print(sf.getSequence()[i]+"  ");
         }
+        System.out.println();
+        Point point=null ;
+//        System.out.println(point.bigI);
+        new Game();
     }
 }
--- src/main/java/SmField.java
+++ src/main/java/SmField.java
@@ -1,45 +1,74 @@
+package main.java;
+
 import static java.lang.Math.abs;
 
 public class SmField {
-    private int smallState;
+    //    emount of free cells
+    private int freeCells;
+    //  0 - not occupied    -1 - occ by ZERO    1 - occ by CROSS
+    private int stateSm;
+    //  game cells
     private int[][] cellsOfField;
+    //  sequence for this smallField. Contains sum for all direction in smallField. Let you see java.java.Sta.
     private int[] sequence;
 
     public SmField() {
-        this.smallState = 0;
+        freeCells = 9;
+        this.stateSm = 0;
         this.cellsOfField = new int[3][3];
         sequence = new int[8];
     }
 
-    public boolean setXij(int i, int j) {
-        return setIJ(i, j, 1);
+    public boolean isCellFree(int i, int j) {
+        return cellsOfField[i][j] == 0;
     }
 
+    //  CROSS step. return true if success
     public boolean setOij(int i, int j) {
         return setIJ(i, j, -1);
     }
 
+    //  ZERO step. return true if success
+    public boolean setXij(int i, int j) {
+        return setIJ(i, j, 1);
+    }
+
+    //  step by ZERO or CROSS
     private boolean setIJ(int i, int j, int XO) {
-        if (cellsOfField[i][j] != 0) return false;
+//        if (cellsOfField[i][j] != 0) return false;
+        boolean boTmp = false;
+        freeCells--;
         cellsOfField[i][j] += XO;
-        int tmp[] = Sta.cellSeq[i][j];
-        int state = 0;
-        for (int k : tmp) {
-            sequence[k] += XO;
-            if (XO == 1) {
-                if (sequence[k] > state) state = sequence[k];
-            } else
-                if (sequence[k] < state) state = sequence[k];
+//      if smallField is not occupied, recount current sequence.
+        if (stateSm == 0) {
+            int tmp[] = Sta.cellSeq[i][j];
+            int stateTmp = 0;
+            for (int l = 0; l < tmp.length; l++) {
+                int k = tmp[l];
+                sequence[k] += XO;
+                if (XO == 1) {
+                    if (sequence[k] > stateTmp) stateTmp = sequence[k];
+                } else if (sequence[k] < stateTmp) stateTmp = sequence[k];
+            }
+            if (abs(stateTmp) == 3) {
+                this.stateSm = XO;
+                boTmp = true;
+            }
         }
-        if (abs(state) == 3) smallState = XO;
-        return true;
+        return boTmp;
     }
 
-
-    public int getSmallState() {
-        return smallState;
+    //  current state
+    public int getStateSm() {
+        return stateSm;
     }
 
+    //  is all cells occupied
+    public boolean isFull() {
+        return freeCells == 0;
+    }
+
+    //    for debugging only
     public int[] getSequence() {
         return sequence;
     }
