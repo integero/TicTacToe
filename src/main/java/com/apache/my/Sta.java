240d0d9b19978100ed65dcee65df1dd6f527679f
Valeryi Tselishchev <var.tsel@gmail.com>
2018-02-28 18:40:09 +0300

    add comments to class Sta

--- src/main/java/com/apache/my/Sta.java
+++ src/main/java/com/apache/my/Sta.java
@@ -1,7 +1,7 @@
 package com.apache.my;
 
 import javafx.scene.paint.Color;
-
+//  game static object common for big & small fields
 class Sta {
 //                              [0-7] - index of sum & count arrays
 //            []  []  []  7     for example, changes in cell[2][0]
@@ -9,17 +9,19 @@ class Sta {
 //            []  []  []  5
 //          0  1   2   3  4
 
+//  set of indexes of sequence array for all sells
     final static int[][][] cellSeq = new int[][][]{
-            {{1, 4, 7}, {1, 6}, {0, 1, 5}},
-            {{2, 7}, {0, 2, 4, 6}, {2, 5}},
-            {{0, 3, 7}, {3, 6}, {3, 4, 5}}};
+            {{1, 4, 7},  {1, 6},       {0, 1, 5}},
+            {{2, 7},     {0, 2, 4, 6}, {2, 5}   },
+            {{0, 3, 7},  {3, 6},       {3, 4, 5}}};
 
     static int smallSize;       //  size of gamecell
     static int smallDx;         //  distance between cells
     static int bigSize;         //  size of small game field. depends of smallSize & smallDx
-    private static int bigDx;   //  distance between small game fields
-    static int XO;              //  current player sign
+    private static int bigDx;   //  distance between small game fields in big field
+    static int XO;              //  current player sign   1 - X   -1 - 0
     static boolean finish;      //  flag of end of game
+    static  Color[] bigCellClr; //  colors for denoting owners of small fields
 
     static final Color clrBack = Color.BROWN;           //  color of background
     static final Color clrCell = Color.GOLD;            //  color of non active cells
@@ -31,7 +33,6 @@ class Sta {
     static final Color clrTxt0 = Color.BLUE;            //  color for possibles steps
     static final Color clrTxt0w = Color.DARKBLUE;       //  color of was maiden O
 
-    static  Color[] bigCellClr;
 //  never used but...
     int[] getSeq(int i, int j) {
         return cellSeq[i][j];
@@ -46,9 +47,9 @@ class Sta {
         bigDx = 4;
         bigField = new SmField[3][3];
         bigCellClr = new Color[3];
-        bigCellClr[0] = Color.LIGHTBLUE;
-        bigCellClr[1] = Color.GOLD;
-        bigCellClr[2] = Color.ORANGERED;
+        bigCellClr[0] = Color.LIGHTBLUE;    //  color for 0 owner
+        bigCellClr[1] = Color.GOLD;         //  color for no owner
+        bigCellClr[2] = Color.ORANGERED;    //  color for X owner
 
         for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) bigField[i][j] = new SmField();
     }
